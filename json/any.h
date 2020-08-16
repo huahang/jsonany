5c488f6c4c976f528a77113888c8c5a2ee3c235c
Hans <hans@dkmt.io>
2020-08-16 12:05:42 +0800

    dump
    Signed-off-by: Hans <hans@dkmt.io>

--- json/any.h
+++ json/any.h
@@ -10,7 +10,7 @@
 
 #ifndef RAPIDJSON_HAS_STDSTRING
 #define RAPIDJSON_HAS_STDSTRING 1
-#endif  // RAPIDJSON_HAS_STDSTRING
+#endif // RAPIDJSON_HAS_STDSTRING
 
 //#ifndef BOOST_HANA_CONFIG_DISABLE_CONCEPT_CHECKS
 //#define BOOST_HANA_CONFIG_DISABLE_CONCEPT_CHECKS 1
@@ -32,20 +32,25 @@
 
 namespace json {
 
+typedef rapidjson::Writer<rapidjson::StringBuffer> Writer;
+
 class Any;
 
-template <typename ValueType>
-const ValueType& AnyCast(const Any& a);
+template <typename ValueType> const ValueType &AnyCast(const Any &a);
+
+template <typename T> auto Dump(const T &o, Writer &w) -> decltype(o.Dump(w));
+
+template <typename T> void Dump(const std::vector<T> &, Writer &);
 
 template <typename T>
-void Serialize(const T& o, rapidjson::Writer<rapidjson::StringBuffer>& w);
+std::enable_if_t<boost::hana::Struct<T>::value, void> Dump(const T &, Writer &);
 
 class Any final {
- public:
+public:
   // constructors
   Any() : holder(nullptr) {}
 
-  Any(const Any& a) : Any() {
+  Any(const Any &a) : Any() {
     if (a.holder == nullptr) {
       this->holder = nullptr;
       return;
@@ -53,7 +58,7 @@ class Any final {
     this->holder = a.holder->Clone();
   }
 
-  Any(Any&& a) : Any() { this->Swap(a); }
+  Any(Any &&a) : Any() { this->Swap(a); }
 
   // destructor
   ~Any() {
@@ -63,27 +68,26 @@ class Any final {
   }
 
   // assignment operator
-  Any& operator=(const Any& a) {
+  Any &operator=(const Any &a) {
     Any copy(a);
     return this->Swap(copy);
   }
 
   // constructors from value
   template <typename ValueType>
-  Any(const ValueType& v) : holder(new ValueHolder<ValueType>(v)) {}
+  Any(const ValueType &v) : holder(new ValueHolder<ValueType>(v)) {}
 
-  Any(const char* str)
+  Any(const char *str)
       : holder(new ValueHolder<std::string>(std::string(str))) {}
 
   // assignment from value
-  template <typename ValueType>
-  Any& operator=(const ValueType& v) {
+  template <typename ValueType> Any &operator=(const ValueType &v) {
     Any a(v);
     return this->Swap(a);
   }
 
   // get type info
-  const std::type_info& TypeInfo() const {
+  const std::type_info &TypeInfo() const {
     return holder != nullptr ? holder->TypeInfo() : typeid(void);
   }
 
@@ -91,86 +95,107 @@ class Any final {
   const bool IsNull() const { return holder == nullptr; }
 
   // get value pointer
-  template <typename ValueType>
-  const ValueType* ValuePointer() const {
-    const auto& holderType = TypeInfo();
-    const auto& valueType = typeid(ValueType);
+  template <typename ValueType> const ValueType *ValuePointer() const {
+    const auto &holderType = TypeInfo();
+    const auto &valueType = typeid(ValueType);
     if (holderType != valueType) {
       return nullptr;
     }
-    return &static_cast<ValueHolder<ValueType>*>(holder)->value;
+    return &static_cast<ValueHolder<ValueType> *>(holder)->value;
   }
 
   // swap
-  Any& Swap(Any& a) {
+  Any &Swap(Any &a) {
     std::swap(holder, a.holder);
     return *this;
   }
 
- private:
+  // serialization
+  void Dump(Writer &w) const { holder->Dump(w); }
+
+private:
   class HolderInterface {
-   public:
-    virtual HolderInterface* Clone() const = 0;
-    virtual const std::type_info& TypeInfo() const = 0;
+  public:
+    virtual HolderInterface *Clone() const = 0;
+    virtual const std::type_info &TypeInfo() const = 0;
+    virtual void Dump(Writer &) const = 0;
     virtual ~HolderInterface() {}
   };
 
-  template <typename ValueType>
-  class ValueHolder : public HolderInterface {
-   public:
-    ValueHolder(const ValueType& v) : value(v) {}
+  template <typename ValueType> class ValueHolder : public HolderInterface {
+  public:
+    ValueHolder(const ValueType &v) : value(v) {}
 
-    ValueHolder(ValueType&& v) : value(v) {}
+    virtual HolderInterface *Clone() const { return new ValueHolder(value); };
 
-    virtual HolderInterface* Clone() const { return new ValueHolder(value); };
+    virtual const std::type_info &TypeInfo() const { return typeid(ValueType); }
 
-    virtual const std::type_info& TypeInfo() const { return typeid(ValueType); }
-
-    //        virtual auto Serialize() const -> decltype(ValueType()) {
-    //          return value;
-    //        }
+    virtual void Dump(Writer &w) const {
+      w.String("(ValueHolder)");
+      // json::Dump(value, w);
+    };
 
     const ValueType value;
   };
 
-  HolderInterface* holder;
+  HolderInterface *holder;
 };
 
-template <typename ValueType>
-const ValueType& AnyCast(const Any& a) {
-  const ValueType* result = a.ValuePointer<ValueType>();
+template <typename ValueType> const ValueType &AnyCast(const Any &a) {
+  const ValueType *result = a.ValuePointer<ValueType>();
   if (result != nullptr) {
     return *result;
   }
   throw std::bad_cast();
 }
 
+inline void Dump(const Any &any, Writer &w) { any.Dump(w); }
+
+inline void Dump(const bool b, Writer &w) { w.Bool(b); }
+
+inline void Dump(const int i, Writer &w) { w.Int(i); }
+
+inline void Dump(const int64_t i, Writer &w) { w.Int64(i); }
+
+inline void Dump(const std::string &s, Writer &w) { w.String(s); }
+
+template <typename T> auto Dump(const T &o, Writer &w) -> decltype(o.Dump(w)) {
+  o.Dump(w);
+}
+
 template <typename T>
-void Serialize(const T& o, rapidjson::Writer<rapidjson::StringBuffer>& w) {
+std::enable_if_t<boost::hana::Struct<T>::value, void> Dump(const T &o,
+                                                           Writer &w) {
   namespace hana = boost::hana;
   w.StartObject();
-  auto accessors = hana::accessors<T>();
-  boost::hana::for_each(accessors, [&o, &w](auto accessor) {
-    auto key = boost::hana::first(accessor);
-    auto get = boost::hana::second(accessor);
-    auto value = get(o);
-    w.String(key.c_str());
-    auto& typeInfo = typeid(value);
-    if (typeInfo == typeid(std::string)) {
-      w.String(reinterpret_cast<const std::string&>(value));
-    } else if (typeInfo == typeid(int)) {
-      w.Int(reinterpret_cast<const int&>(value));
-    } else if (typeInfo == typeid(int64_t)) {
-      w.Int64(reinterpret_cast<const int64_t&>(value));
-    } else if (typeInfo == typeid(json::Any)) {
-      w.String("(json::Any)");
-    } else {
-      w.String("(unknown)");
-    }
+  boost::hana::for_each(boost::hana::keys(o), [&](auto name) {
+    auto const &member = boost::hana::at_key(o, name);
+    w.String(boost::hana::to<char const *>(name));
+    Dump(member, w);
   });
   w.EndObject();
 }
 
-}  // namespace json
+template <typename T> void Dump(const std::vector<T> &array, Writer &w) {
+  w.StartArray();
+  for (const auto &item : array) {
+    Dump(item, w);
+  }
+  w.EndArray();
+}
 
-#endif  // JSON_ANY_H
+// template <typename T> void Dump(const T & o, Writer &w) {
+//  w.String("(Unknown)");
+//}
+
+template <typename T>
+std::enable_if_t<boost::hana::Struct<T>::value, std::string> Dump(const T &o) {
+  rapidjson::StringBuffer sb;
+  Writer w(sb);
+  Dump(o, w);
+  return sb.GetString();
+}
+
+} // namespace json
+
+#endif // JSON_ANY_H
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 #include <stdlib.h>
 
+#include <algorithm>
 #include <boost/fusion/include/vector.hpp>
 #include <boost/hana.hpp>
 #include <boost/hana/ext/boost/fusion/vector.hpp>
@@ -12,7 +13,6 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 #include "json/any.h"
 
@@ -23,37 +23,36 @@ struct Person;
 
 struct Singer {
   BOOST_HANA_DEFINE_STRUCT(Singer, (std::string, type), (int, age));
+  void Dump(json::Writer &w) { json::Dump(*this, w); }
 };
 
 struct Band {
   BOOST_HANA_DEFINE_STRUCT(Band, (std::vector<Singer>, singers));
+  void Dump(json::Writer &w) { json::Dump(*this, w); }
 };
 
 struct Address {
-  BOOST_HANA_DEFINE_STRUCT(Address,
-                           (std::string, country),
-                           (std::string, city),
+  BOOST_HANA_DEFINE_STRUCT(Address, (std::string, country), (std::string, city),
                            (std::string, street),
                            (std::vector<Person>, neighbors));
+  void Dump(json::Writer &w) { json::Dump(*this, w); }
 };
 
 struct Friend {
-  BOOST_HANA_DEFINE_STRUCT(Friend,
-                           (std::string, relation),
+  BOOST_HANA_DEFINE_STRUCT(Friend, (std::string, relation),
                            (json::Any, secret));
+  void Dump(json::Writer &w) { json::Dump(*this, w); }
 };
 
 struct Person {
-  BOOST_HANA_DEFINE_STRUCT(Person,
-                           (std::string, name),
-                           (int, age),
-                           (Address, address),
-                           (std::vector<Friend>, friends),
+  BOOST_HANA_DEFINE_STRUCT(Person, (std::string, name), (int, age),
+                           (Address, address), (std::vector<Friend>, friends),
                            (json::Any, secret));
+  void Dump(json::Writer &w) { json::Dump(*this, w); }
 };
 
 template <typename T>
-void Serialize(const T& o, rapidjson::Writer<rapidjson::StringBuffer>& w) {
+void Serialize(const T &o, rapidjson::Writer<rapidjson::StringBuffer> &w) {
   namespace hana = boost::hana;
   w.StartObject();
   auto accessors = hana::accessors<T>();
@@ -62,39 +61,39 @@ void Serialize(const T& o, rapidjson::Writer<rapidjson::StringBuffer>& w) {
     auto get = boost::hana::second(accessor);
     auto value = get(o);
     w.String(key.c_str());
-    auto& typeInfo = typeid(value);
+    auto &typeInfo = typeid(value);
     if (typeInfo == typeid(std::string)) {
-      w.String(reinterpret_cast<const std::string&>(value));
+      w.String(reinterpret_cast<const std::string &>(value));
     } else if (typeInfo == typeid(int)) {
-      w.Int(reinterpret_cast<const int&>(value));
+      w.Int(reinterpret_cast<const int &>(value));
     } else if (typeInfo == typeid(int64_t)) {
-      w.Int64(reinterpret_cast<const int64_t&>(value));
+      w.Int64(reinterpret_cast<const int64_t &>(value));
     } else if (typeInfo == typeid(Singer)) {
-      Serialize(reinterpret_cast<const Singer&>(value), w);
+      Serialize(reinterpret_cast<const Singer &>(value), w);
     } else if (typeInfo == typeid(Address)) {
-      Serialize(reinterpret_cast<const Address&>(value), w);
+      Serialize(reinterpret_cast<const Address &>(value), w);
     } else if (typeInfo == typeid(Friend)) {
-      Serialize(reinterpret_cast<const Friend&>(value), w);
+      Serialize(reinterpret_cast<const Friend &>(value), w);
     } else if (typeInfo == typeid(Person)) {
-      Serialize(reinterpret_cast<const Person&>(value), w);
+      Serialize(reinterpret_cast<const Person &>(value), w);
     } else if (typeInfo == typeid(std::vector<Friend>)) {
-      const std::vector<Friend>& friends =
-          reinterpret_cast<const std::vector<Friend>&>(value);
+      const std::vector<Friend> &friends =
+          reinterpret_cast<const std::vector<Friend> &>(value);
       w.StartArray();
-      for (const auto& f : friends) {
+      for (const auto &f : friends) {
         Serialize(f, w);
       }
       w.EndArray();
     } else if (typeInfo == typeid(std::vector<Person>)) {
-      const std::vector<Person>& persons =
-          reinterpret_cast<const std::vector<Person>&>(value);
+      const std::vector<Person> &persons =
+          reinterpret_cast<const std::vector<Person> &>(value);
       w.StartArray();
-      for (const auto& p : persons) {
+      for (const auto &p : persons) {
         Serialize(p, w);
       }
       w.EndArray();
     } else if (typeInfo == typeid(json::Any)) {
-      json::Any& any = reinterpret_cast<json::Any&>(value);
+      json::Any &any = reinterpret_cast<json::Any &>(value);
       if (any.TypeInfo() == typeid(std::string)) {
         w.String(json::AnyCast<std::string>(any));
       } else if (any.TypeInfo() == typeid(int)) {
@@ -121,8 +120,7 @@ void Serialize(const T& o, rapidjson::Writer<rapidjson::StringBuffer>& w) {
   w.EndObject();
 }
 
-template <typename T>
-std::string Serialize(const T& o) {
+template <typename T> std::string Serialize(const T &o) {
   using rapidjson::PrettyWriter;
   using rapidjson::StringBuffer;
   StringBuffer sb;
@@ -131,8 +129,7 @@ std::string Serialize(const T& o) {
   return sb.GetString();
 }
 
-template <typename T>
-T Parse(const std::string& json) {
+template <typename T> T Parse(const std::string &json) {
   namespace hana = boost::hana;
   using rapidjson::Document;
   Document document;
@@ -148,32 +145,24 @@ T Parse(const std::string& json) {
   return result;
 }
 
-template <typename Xs>
-std::string join(Xs&& xs, std::string sep) {
+template <typename Xs> std::string join(Xs &&xs, std::string sep) {
   return boost::hana::fold(boost::hana::intersperse(std::forward<Xs>(xs), sep),
                            "", boost::hana::_ + boost::hana::_);
 }
 
-std::string quote(std::string s) {
-  return "\"" + s + "\"";
-}
+std::string quote(std::string s) { return "\"" + s + "\""; }
 
-template <typename T>
-auto to_json(T const& x) -> decltype(std::to_string(x)) {
+template <typename T> auto to_json(T const &x) -> decltype(std::to_string(x)) {
   return std::to_string(x);
 }
 
-std::string to_json(char c) {
-  return quote({c});
-}
+std::string to_json(char c) { return quote({c}); }
 
-std::string to_json(std::string s) {
-  return quote(s);
-}
+std::string to_json(std::string s) { return quote(s); }
 
 template <typename X>
-std::enable_if_t<boost::hana::Struct<X>::value, std::string> to_json(
-  std::vector<X> const& xs) {
+std::enable_if_t<boost::hana::Struct<X>::value, std::string>
+to_json(std::vector<X> const &xs) {
   std::string json = "[";
   for (int i = 0; i < xs.size(); i++) {
     json.append(to_json(xs[i]));
@@ -187,33 +176,33 @@ std::enable_if_t<boost::hana::Struct<X>::value, std::string> to_json(
 }
 
 template <typename Xs>
-std::enable_if_t<boost::hana::Sequence<Xs>::value, std::string> to_json(
-    Xs const& xs) {
+std::enable_if_t<boost::hana::Sequence<Xs>::value, std::string>
+to_json(Xs const &xs) {
   auto json =
-      boost::hana::transform(xs, [](auto const& x) { return to_json(x); });
+      boost::hana::transform(xs, [](auto const &x) { return to_json(x); });
   return "[" + join(std::move(json), ", ") + "]";
 }
 
 template <typename T>
-std::enable_if_t<boost::hana::Struct<T>::value, std::string> to_json(
-    T const& x) {
+std::enable_if_t<boost::hana::Struct<T>::value, std::string>
+to_json(T const &x) {
   auto json = boost::hana::transform(boost::hana::keys(x), [&](auto name) {
-    auto const& member = boost::hana::at_key(x, name);
-    return quote(boost::hana::to<char const*>(name)) + " : " + to_json(member);
+    auto const &member = boost::hana::at_key(x, name);
+    return quote(boost::hana::to<char const *>(name)) + " : " + to_json(member);
   });
   return "{" + join(std::move(json), ", ") + "}";
 }
 
 template <typename T>
-std::enable_if_t<std::is_same<T, int>::value, T> from_json(std::istream& in) {
+std::enable_if_t<std::is_same<T, int>::value, T> from_json(std::istream &in) {
   T result;
   in >> result;
   return result;
 }
 
 template <typename T>
-std::enable_if_t<std::is_same<T, std::string>::value, T> from_json(
-    std::istream& in) {
+std::enable_if_t<std::is_same<T, std::string>::value, T>
+from_json(std::istream &in) {
   char quote;
   in >> quote;
 
@@ -226,14 +215,14 @@ std::enable_if_t<std::is_same<T, std::string>::value, T> from_json(
 }
 
 template <typename T>
-std::enable_if_t<hana::Struct<T>::value, T> from_json(std::istream& in) {
+std::enable_if_t<hana::Struct<T>::value, T> from_json(std::istream &in) {
   T result;
   char brace;
   in >> brace;
 
   hana::for_each(hana::keys(result), [&](auto key) {
     in.ignore(std::numeric_limits<std::streamsize>::max(), ':');
-    auto& member = hana::at_key(result, key);
+    auto &member = hana::at_key(result, key);
     using Member = std::remove_reference_t<decltype(member)>;
     member = from_json<Member>(in);
   });
@@ -242,7 +231,7 @@ std::enable_if_t<hana::Struct<T>::value, T> from_json(std::istream& in) {
 }
 
 template <typename Xs>
-std::enable_if_t<hana::Sequence<Xs>::value, Xs> from_json(std::istream& in) {
+std::enable_if_t<hana::Sequence<Xs>::value, Xs> from_json(std::istream &in) {
   Xs result;
   char bracket;
   in >> bracket;
@@ -252,7 +241,7 @@ std::enable_if_t<hana::Sequence<Xs>::value, Xs> from_json(std::istream& in) {
       in >> comma;
     }
 
-    auto& element = hana::at(result, i);
+    auto &element = hana::at(result, i);
     using Element = std::remove_reference_t<decltype(element)>;
     element = from_json<Element>(in);
   });
@@ -262,38 +251,46 @@ std::enable_if_t<hana::Sequence<Xs>::value, Xs> from_json(std::istream& in) {
 
 int main() {
   std::string json;
+  // Singer
   Singer s1{"rapper", 16};
-  json = Serialize(s1);
+  json = json::Dump(s1);
   std::cout << json << std::endl;
-  std::istringstream istream(json);
-  Singer s1Parsed = from_json<Singer>(istream);
-  json = to_json(s1);
+  // Band
+  Band band{{s1, s1}};
+  json = json::Dump(band);
   std::cout << json << std::endl;
-
+  // Friend
   Friend f1{"my best friend", Singer{"rocker", 18}};
-  std::cout << Serialize(f1) << std::endl;
+  json = json::Dump(f1);
+  std::cout << json << std::endl;
 
-  Band band{{s1,s1}};
-  // band.singers.assign_sequence
-  // band.singers.push_back(s1);
-  std::cout << to_json(band) << std::endl;
-
-  auto& f1Secret = json::AnyCast<Singer>(f1.secret);
-  std::cout << Serialize(f1Secret) << std::endl;
-  Friend f2{"new friend", "little girl"};
-  std::cout << Serialize(f2) << std::endl;
-  auto& f2Secret = json::AnyCast<std::string>(f2.secret);
-  Friend f3{"third friend", 3};
-  std::cout << Serialize(f3) << std::endl;
-  auto& f3Secret = json::AnyCast<int>(f3.secret);
-  Person p2{"p2", 3, Address{"china", "shanghai", "putuo"}};
-  std::cout << Serialize(p2) << std::endl;
-  auto& p2SecretType = p2.secret.TypeInfo();
-  Address addr1{"china", "beijing", "wangjing", {p2}};
-  std::cout << Serialize(addr1) << std::endl;
-  Person p1{"p1", 4, addr1, {f1, f2, f3}, "the kind!"};
-  auto& p1Secret = json::AnyCast<std::string>(p1.secret);
-  std::cout << Serialize(p1) << std::endl;
+  //  std::istringstream istream(json);
+  //  Singer s1Parsed = from_json<Singer>(istream);
+  //  json = to_json(s1);
+  //  std::cout << json << std::endl;
+  //
+  //
+  //  Band band{{s1,s1}};
+  //  // band.singers.assign_sequence
+  //  // band.singers.push_back(s1);
+  //  std::cout << to_json(band) << std::endl;
+  //
+  //  auto& f1Secret = json::AnyCast<Singer>(f1.secret);
+  //  std::cout << Serialize(f1Secret) << std::endl;
+  //  Friend f2{"new friend", "little girl"};
+  //  std::cout << Serialize(f2) << std::endl;
+  //  auto& f2Secret = json::AnyCast<std::string>(f2.secret);
+  //  Friend f3{"third friend", 3};
+  //  std::cout << Serialize(f3) << std::endl;
+  //  auto& f3Secret = json::AnyCast<int>(f3.secret);
+  //  Person p2{"p2", 3, Address{"china", "shanghai", "putuo"}};
+  //  std::cout << Serialize(p2) << std::endl;
+  //  auto& p2SecretType = p2.secret.TypeInfo();
+  //  Address addr1{"china", "beijing", "wangjing", {p2}};
+  //  std::cout << Serialize(addr1) << std::endl;
+  //  Person p1{"p1", 4, addr1, {f1, f2, f3}, "the kind!"};
+  //  auto& p1Secret = json::AnyCast<std::string>(p1.secret);
+  //  std::cout << Serialize(p1) << std::endl;
 
   // TODO. 以下是伪代码，需要笔试者具体实现 // auto json = dump(p1)
   // std::cout << json << std::endl // std::cout << p1 << std::endl
